de7bec936425ab1a3a725dfcd992096b9b410dbd
Pedro de Carvalho <web@43.lc>
2014-12-21 16:09:19 +0000

    cleaning up comments

--- last-logins.bash
+++ last-logins.bash
@@ -20,11 +20,8 @@ today_sessions=()
 yesterday_sessions=()
 
 while read session; do
-#	read USER TTY EDAY_OR_HOST TIME <<< $(echo "$session" | awk '{ print $1, $2, $11, $14 }')
-#	read USER TTY WEEKDAY MONTH DAY HOUR DIFFF EHOUR DURATION HOST <<< $(echo "$session" | awk '{ print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15 }')
 	read USER TTY DAY EDAY_OR_HOST TIME <<< $(echo "$session" | awk '{ print $1, $2, $5 , $11, $14 }')
 	if [[ "$HOST" == ":0" || "$HOST" == ":0.0" ]];then
-		# Ignore "local" logins
 		continue;
 	elif grep -q "still logged in" <<<$session; then
 		if [[ "/dev/$TTY" == "$current_tty" ]]; then
@@ -43,18 +40,6 @@ while read session; do
 		yesterday_sessions+=("$session")
  	fi
 done < <(last -Fiwa $USER | head -n -2  )     # | tail -n +2 ) 
-
-	# ----- LAST LOGIN
-
-#read USER HOUR EHOUR DURATION HOST <<< $(echo "$yesterday_sessions" | awk '{ print $1, $6, $7, $9, $10 }')
-
-#Echo -e "Yesterday, these where the connections made to this machine: "
-#echo -e "$USER at $HOST logged in for $DURATION, from $HOUR to $EHOUR";
-
-#read USER HOUR EHOUR DURATION HOST <<< $(echo "$today_sessions" | awk '{ print $1, $6, $7, $9, $10 }')
-
-#echo -e "Yesterday, these where the connections made to this machine: "
-#echo -e "$USER at $HOST logged in for $DURATION, from $HOUR to $EHOUR";
 
 echo -e "\e[38;5;174mTODAY${RESET}, these where the connections made to this machine: "
 
@@ -78,18 +63,13 @@ if [ ${#today_sessions[@]} -gt 0 ]; then
 			ECHO_PERIOD="from $HOUR to $EHOUR";
 			ECHO_DURATION=" for "
 		fi
-
-
-#		printf "${BOLD}\e[38;5;205m$USER${RESET}at \e[38;5;38m$HOST${RESET} logged in $ECHO_DURATION \e[38;5;195m$DURATION${RESET}, $ECHO_PERIOD\n"
 		printf "${BOLD}\e[38;5;205m%-10s${RESET} at \e[38;5;38m%s${RESET} logged in %s \e[38;5;195m%s${RESET}, $ECHO_PERIOD\n" "$USER" "$HOST" "$ECHO_DURATION" "$DURATION"
 	done
 fi
 
 
 if [[ ${#yesterday_sessions[@]} -eq 0 ]]; then
-	# ----- NO LOGINS YESTERDAY 
 	echo -e "No logins were registered \e[38;5;174myesterday!${RESET}";
-#	exit 0;
 else 
  	echo -e "\e[38;5;174mYESTERDAY${RESET}, these where the connections made to this machine: "
   for yesterday_session in "${yesteday_sessions[@]}"
@@ -99,33 +79,12 @@ else
 	done
 fi
 
-
-#	read USER TTY HOST TIME <<< $(echo "$session" | awk '{ print $1, $2, $3, $15 }')
-#	read USER TTY EDAY_OR_HOST TIME <<< $(echo "$session" | awk '{ print $1, $2, $11, $14 }')
-#	if [[ "$HOST" == ":0" || "$HOST" == ":0.0" ]];then
-		# Ignore "local" logins
-#		continue;
-#	elif grep -q "still logged in" <<<$session; then
-#		if [[ "/dev/$TTY" == "$current_tty" ]]; then
-#			current_session="$session"
-#		else
-#			live_sessions+=("$session")
-#		fi
-#	else
-#		dead_sessions+=("$session")
-#	fi
-  # recent logins  # junk data at end # Hide the current login
-#done < <(last -Fiwa $USER | head -n -2 )     # | tail -n +2 ) 
-
 if [ ${#live_sessions[@]} -gt 0 ]; then
-	# ----- CURRENT LOGINS
   echo -e "You are also ${BOLD}${RED}still logged in${RESET} from the following machines:"
   for session in "${live_sessions[@]}"
 	do
-#		read USER TTY HOST TIME <<< $(echo "$session" | awk '{ print $1, $2, $3, $15 }')
 		read TTY HOST TIME <<< $(echo "$session" | awk '{ print $2, $11 }')
 		LOGIN=$(echo "$session" | awk '{ print $4, $5, $7, $6 }')
-
 		since=`since "$LOGIN"`
 		echo -e "  ${BOLD}*${RESET} Logged in ${BOLD}$since${RESET} from ${BOLD}\e[38;5;38m$HOST${RESET} on ${BOLD}${YELLOW}$TTY${RESET}";
 	done
@@ -139,7 +98,6 @@ elif [[ ${#dead_sessions[@]} -eq 0 ]]; then
 else
 	# ----- LAST LOGIN
 	last_session=${dead_sessions[0]}
-#	read USER TTY HOST TIME <<< $(echo "$last_session" | awk '{ print $1, $2, $3, $15 }')
 	read HOST <<< $(echo "$last_session" | awk '{ print $15 }')
  	LOGIN=$(echo "$last_session" | awk '{ print $5, $6, $8, $7 }')
 	LOGOUT=$(echo "$last_session" | awk '{ print $10, $11, $13, $12 }')
