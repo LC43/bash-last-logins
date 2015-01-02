50fb0628b1ba212a02245b005c5c082e32dd08f3
Pedro de Carvalho <web@43.lc>
2015-01-02 23:11:22 +0000

    fixed yesterday listing, removed duplicate entries, simplified duration

--- last-logins.bash
+++ last-logins.bash
@@ -13,7 +13,6 @@ current_day=`date +"%-d"`
 yesterday=`date --date="yesterday" +"%-d"`
 current_session=""
 
-
 live_sessions=()
 last_sessions=()
 today_sessions=()
@@ -23,6 +22,8 @@ while read session; do
 	read USER TTY DAY EDAY_OR_HOST TIME <<< $(echo "$session" | awk '{ print $1, $2, $5 , $11, $14 }')
 	if [[ "$HOST" == ":0" || "$HOST" == ":0.0" ]];then
 		continue;
+  elif [[ "$DAY" == "${current_day}" ]]; then
+		today_sessions+=("$session")
 	elif grep -q "still logged in" <<<$session; then
 		if [[ "/dev/$TTY" == "$current_tty" ]]; then
 			current_session="$session"
@@ -34,9 +35,9 @@ while read session; do
 	fi
 
 
-	if [[ "$DAY" == $current_day ]]; then
-		today_sessions+=("$session")
-	elif [[ "$DAY" == $yesterday ]]; then
+
+
+	if [[ "$DAY" == "${yesterday}" ]]; then
 		yesterday_sessions+=("$session")
  	fi
 done < <(last -Fiwa $USER | head -n -2  )     # | tail -n +2 ) 
@@ -51,7 +52,7 @@ if [ ${#today_sessions[@]} -gt 0 ]; then
 			HOST=$EDAY_OR_HOST
 			EHOUR="and is \e[1;48;5;1mStill Logged In!${RESET}"
 			LOGIN=$(echo "$today_session" | awk '{ print $4, $5, $7, $6 }')
-			DURATION=`since "$LOGIN"`
+			SIMPLEDURATION=`since "$LOGIN"`
 			PERIOD=""
 			ECHO_DURATION=""
 			ECHO_PERIOD=$EHOUR
@@ -62,8 +63,9 @@ if [ ${#today_sessions[@]} -gt 0 ]; then
 		else
 			ECHO_PERIOD="from $HOUR to $EHOUR";
 			ECHO_DURATION=" for "
+			SIMPLEDURATION=$(sed  's#(\(.*\):\(.*\))#\1h\2#g' <<< $DURATION )
 		fi
-		printf "${BOLD}\e[38;5;205m%-10s${RESET} at \e[38;5;38m%s${RESET} logged in %s \e[38;5;195m%s${RESET}, $ECHO_PERIOD\n" "$USER" "$HOST" "$ECHO_DURATION" "$DURATION"
+		printf "${BOLD}\e[38;5;205m%-10s${RESET} at \e[38;5;38m%s${RESET} logged in %s \e[38;5;195m%s${RESET}, $ECHO_PERIOD\n" "$USER" "$HOST" "$ECHO_DURATION" "${SIMPLEDURATION}"
 	done
 fi
 
@@ -72,10 +74,11 @@ if [[ ${#yesterday_sessions[@]} -eq 0 ]]; then
 	echo -e "No logins were registered \e[38;5;174myesterday!${RESET}";
 else 
  	echo -e "\e[38;5;174mYESTERDAY${RESET}, these where the connections made to this machine: "
-  for yesterday_session in "${yesteday_sessions[@]}"
+  for yesterday_session in "${yesterday_sessions[@]}"
 	do
-		read USER HOUR EHOUR DURATION HOST <<< $(echo "$yesterday_session" | awk '{ print $1, $6, $7, $9, $10 }')
-		echo -e "${BOLD}$USER${RESET} at ${BOLD}$HOST${RESET} logged in for ${BOLD}{RED}$DURATION${RESET}, from {YELLOW}$HOUR to $EHOUR${RESET}";
+		read USER TTY HOUR DASH_OR_STILL EDAY_OR_HOST EHOUR DURATION HOST <<< $(echo "$yesterday_session" | awk '{ print $1, $2, $6, $8, $11, $12, $14, $15}')
+		SIMPLEDURATION=$(sed  's#(\(.*\):\(.*\))#\1h\2#g'<<< $DURATION )
+		echo -e "${BOLD}$USER${RESET} at ${BOLD}$HOST${RESET} logged in for ${BOLD}${RED}$SIMPLEDURATION${RESET}, from $HOUR to $EHOUR$";
 	done
 fi
 
@@ -92,15 +95,14 @@ if [ ${#live_sessions[@]} -gt 0 ]; then
 elif [[ ${#dead_sessions[@]} -eq 0 ]]; then
 	# ----- NO CURRENT OR PREVIOUS LOGINS
 	echo -e "Welcome, this is your ${BOLD}first time${RESET} logging in!";
-	echo -e "If this is not your first time logging in, someone has tampered with the logs!";
-	exit 0;
+	echo -e "If this is not your first time logging in, your logs have been rotated or someone has tampered with the logs!";
 	
-else
+#else
 	# ----- LAST LOGIN
-	last_session=${dead_sessions[0]}
-	read HOST <<< $(echo "$last_session" | awk '{ print $15 }')
- 	LOGIN=$(echo "$last_session" | awk '{ print $5, $6, $8, $7 }')
-	LOGOUT=$(echo "$last_session" | awk '{ print $10, $11, $13, $12 }')
-	echo -e "Last logged in from ${BOLD}$LOGIN${RESET} to ${BOLD}$LOUGOUT${RESET} from ${BOLD}${YELLOW}$HOST${RESET}"
+#	last_session=${dead_sessions[0]}
+#	read HOST <<< $(echo "$last_session" | awk '{ print $15 }')
+# 	LOGIN=$(echo "$last_session" | awk '{ print $5, $6, $8, $7 }')
+#	LOGOUT=$(echo "$last_session" | awk '{ print $10, $11, $13, $12 }')
+#	echo -e "Last logged in from ${BOLD}$LOGIN${RESET} to ${BOLD}$LOUGOUT${RESET} from ${BOLD}${YELLOW}$HOST${RESET}"
 	
 fi
